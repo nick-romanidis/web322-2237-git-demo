9359d4a6d85d503a268171361d95565f94f0b19c
Nick Romanidis <nick.romanidis@senecacollege.ca>
2023-10-12 12:38:18 -0400

    Jane now works for Scotiabank

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
             name: "Jane",
             age: 33,
             occupation: "Manager",
-            company: "RBC",
+            company: "Scotiabank",
             visible: true
         }
     ];
